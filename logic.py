16f5816527619c41236a9beb929712d5f6c8db9e
Lowka227 <rinatbiktagirov56@gmail.com>
2025-08-08 13:50:11 +0300

    third commit

--- logic.py
+++ logic.py
@@ -11,6 +11,7 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.abil = self.get_abil()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -33,12 +34,24 @@ class Pokemon:
             return (data['name'])
         else:
             return "Pikachu"
-
-
+        
+    def get_abil(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['abilities'][0]['ability']['name'])
+        else:
+            return "Pikachu"
+        
     # Метод класса для получения информации
     def info(self):
         return f"Имя твоего покемона: {self.name}"
 
+    # Метод класса для получения способностей покемона
+    def show_abil(self):
+        return f"Способности: {self.abil}"
+    
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
